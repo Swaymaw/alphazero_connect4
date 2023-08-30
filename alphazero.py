19ec7a6c4a9e86bc5479417fdc8f012673234f6d
Swaymaw <swaymaw@gmail.com>
2023-08-30 18:05:25 +0530

    made a working alphazero training loop and also trained it for 3 iterations

---
+++ alphazero.py
@@ -0,0 +1,83 @@
+from mcts import MCTS
+import torch
+import numpy as np
+from tqdm import trange
+import random
+import torch.nn.functional as F
+
+
+class AlphaZero:
+    def __init__(self, model, optimizer, game, args):
+        self.model = model
+        self.optimizer = optimizer
+        self.game = game
+        self.args = args
+        self.mcts = MCTS(game, args, model)
+
+    def self_play(self):
+        memory = []
+        player = 1
+        state = self.game.get_initial_state()
+
+        while True:
+            neutral_state = self.game.change_perspective(state, player)
+            action_probs = self.mcts.search(neutral_state)
+
+            memory.append((neutral_state, action_probs, player))
+
+            action = np.random.choice(self.game.action_size, p=action_probs)
+
+            state = self.game.get_next_state(state, action, player)
+
+            value, is_terminal = self.game.get_value_and_terminated(state, action)
+
+            if is_terminal:
+                return_memory = []
+                for hist_neutral_state, hist_action_probs, hist_player in memory:
+                    hist_outcome = value if hist_player == player else self.game.get_opponent_value(value)
+                    return_memory.append((
+                        self.game.get_encoded_state(hist_neutral_state),
+                        hist_action_probs,
+                        hist_outcome
+                    ))
+
+                return return_memory
+            player = self.game.get_opponent(player)
+
+    def train(self, memory):
+        random.shuffle(memory)
+        for batchidx in range(0, len(memory), self.args['batch_size']):
+            sample = memory[batchidx:min(len(memory)-1, batchidx + self.args['batch_size'])]
+            state, policy_targets, value_targets = zip(*sample)
+
+            state, policy_targets, value_targets = (np.array(state), np.array(policy_targets),
+                                                    np.array(value_targets).reshape((-1, 1)))
+
+            state = torch.tensor(state, dtype=torch.float32)
+            policy_targets = torch.tensor(policy_targets, dtype=torch.float32)
+            value_targets = torch.tensor(value_targets, dtype=torch.float32)
+
+            out_policy, out_value = self.model(state)
+
+            policy_loss = F.cross_entropy(out_policy, policy_targets)
+            value_loss = F.mse_loss(out_value, value_targets)
+
+            loss = policy_loss + value_loss
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+    def learn(self):
+        for iter in range(self.args['num_iterations']):
+            memory = []
+            self.model.eval()
+            for self_play_iteration in trange(self.args['self_play_iterations']):
+                memory += self.self_play()
+
+            self.model.train()
+            for epoch in trange(self.args['num_epochs']):
+                self.train(memory)
+
+            torch.save(self.model.state_dict(), f'model_{iter}.pt')
+            torch.save(self.optimizer.state_dict(), f'optimizer_{iter}.pt')
--- main.py
+++ main.py
@@ -2,45 +2,50 @@ from tictactoe import TicTacToe
 from mcts import MCTS
 from model import ResNet
 import numpy as np
+from alphazero import AlphaZero
+import torch
 
 tictactoe = TicTacToe()
-player = 1
+model = ResNet(tictactoe, 4, 64)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
 
 args = {
     'C': 2,
-    'num_searches': 1000
+    'num_searches': 60,
+    'num_iterations': 3,
+    'self_play_iterations': 500,
+    'num_epochs': 4,
+    'batch_size': 64
 }
 
-model = ResNet(tictactoe, 4, 64)
-model.eval()
+alphazero = AlphaZero(model, optimizer, tictactoe, args)
+alphazero.learn()
 
-mcts = MCTS(tictactoe, args, model)
-state = tictactoe.get_initial_state()
-
-while True:
-    print(state)
-
-    if player == 1:
-        valid_moves = tictactoe.get_valid_moves(state)
-        print('valid_moves', [i for i in range(tictactoe.action_size) if valid_moves[i] == 1])
-        action = int(input(f'{player}:'))
-        if valid_moves[action] == 0:
-            print('action not valid')
-            continue
-    else:
-        neutral_state = tictactoe.change_perspective(state, player)
-        mcts_probs = mcts.search(neutral_state)
-        action = np.argmax(mcts_probs)
-
-
-    state = tictactoe.get_next_state(state, action, player)
-    value, is_terminal = tictactoe.get_value_and_terminated(state, action)
-    if is_terminal:
-        print(state)
-        if value == 1:
-            print(player, 'win')
-        else:
-            print('draw')
-        break
-
-    player = tictactoe.get_opponent(player)
+# while True:
+#     print(state)
+#
+#     if player == 1:
+#         valid_moves = tictactoe.get_valid_moves(state)
+#         print('valid_moves', [i for i in range(tictactoe.action_size) if valid_moves[i] == 1])
+#         action = int(input(f'{player}:'))
+#         if valid_moves[action] == 0:
+#             print('action not valid')
+#             continue
+#     else:
+#         neutral_state = tictactoe.change_perspective(state, player)
+#         mcts_probs = mcts.search(neutral_state)
+#         action = np.argmax(mcts_probs)
+#
+#
+#     state = tictactoe.get_next_state(state, action, player)
+#     value, is_terminal = tictactoe.get_value_and_terminated(state, action)
+#     if is_terminal:
+#         print(state)
+#         if value == 1:
+#             print(player, 'win')
+#         else:
+#             print('draw')
+#         break
+#
+#     player = tictactoe.get_opponent(player)
